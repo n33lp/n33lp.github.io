7903bdd1bb2fa14a4af5a06c8573c10db829653d
n33lp <canada0706@gmail.com>
2024-11-04 17:49:59 -0500

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';  // Adjust this if your main component has a different name or path
-import reportWebVitals from './reportWebVitals';  // Include only if you use web vitals
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,5 +11,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you use web vitals, keep this line; otherwise, you can remove it
-reportWebVitals();
